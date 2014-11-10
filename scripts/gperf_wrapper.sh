83365bbfcae87220cec4df0ef22d1faa15ca80dc
Ubuntu <ubuntu@ip-10-31-82-79.ec2.internal>
2014-11-10 14:57:29 +0000

    save pid on program start

--- scripts/gperf_wrapper.sh
+++ scripts/gperf_wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 PROFILE_FILE="$1"
+PROFILE_DIR="`dirname $PROFILE_FILE`"
+mkdir -p "$PROFILE_DIR"
 shift 1
 
-env LD_PRELOAD="/usr/lib/libtcmalloc_and_profiler.so" CPUPROFILE="$PROFILE_FILE" CPUPROFILESIGNAL=12 "$@"
+LD_PRELOAD="/usr/lib/libtcmalloc_and_profiler.so" CPUPROFILE="$PROFILE_FILE" CPUPROFILESIGNAL=12 "$@" &
+echo $! > "$PROFILE_FILE.pid"
 
